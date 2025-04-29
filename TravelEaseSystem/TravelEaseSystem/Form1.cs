ded71b5789b9a1f6299cfc708cb383867f9a7d88
mina-hill <145162688+mina-hill@users.noreply.github.com>
2025-04-29 10:45:06 +0500

    Local Config Setup

--- TravelEaseSystem/TravelEaseSystem/Form1.cs
+++ TravelEaseSystem/TravelEaseSystem/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,23 @@ namespace TravelEaseSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "localconfig.json";
+            //MessageBox.Show("Looking for config at: " + Path.GetFullPath(path));
+
+            var config = LocalConfig.LoadConfig(path);
+            string connectionString = config?.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                MessageBox.Show("Connection string not loaded.");
+                return;
+            }
+
+            MessageBox.Show("Connection string loaded:\n" + connectionString);
         }
     }
 }
---
+++ TravelEaseSystem/TravelEaseSystem/LocalConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TravelEaseSystem
+{
+    public class LocalConfig
+    {
+        public string ConnectionString { get; set; }
+
+        public static LocalConfig LoadConfig(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string jsonContent = File.ReadAllText(filePath);
+                    return JsonConvert.DeserializeObject<LocalConfig>(jsonContent);
+                }
+                else
+                {
+                    Console.WriteLine($"Config file '{filePath}' not found.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading config: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
